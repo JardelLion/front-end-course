a5b4face2c9c6e8962b140687d315d924db7a140
Jardel Lion <businessjbx@gmail.com>
2024-11-07 21:21:54 +0200

    update

--- javascript/js/script.js
+++
@@ -1,8 +0,0 @@
-let h1Name = document.querySelector(".name")
-const btn = document.querySelector("#btn")
-
-btn.addEventListener('click', () => {
-    let novoNome = prompt("Digite aqui o seu novo nome: ")
-    h1Name.innerHTML = novoNome
-})
-
